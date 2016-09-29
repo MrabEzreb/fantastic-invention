bd3a5c407de6ed6c48a36e16762a311a3badc51a
Mrab Ezreb <Mrab Ezreb>
2016-09-28 20:41:11 -0400

    dddddd

---
+++ client/src/main/scala/mrabezreb/darzil/entity/statics/StaticEntity.scala
@@ -0,0 +1,8 @@
+package mrabezreb.darzil.entity.statics
+
+import mrabezreb.darzil.entity.Entity
+
+abstract class StaticEntity(sex: Double, sey: Double, sewidth: Int, seheight: Int) extends Entity(sex, sey, sewidth, seheight) {
+  
+  
+}
---
+++ client/src/main/scala/mrabezreb/darzil/entity/statics/Tree.scala
@@ -0,0 +1,13 @@
+package mrabezreb.darzil.entity.statics
+
+import mrabezreb.darzil.tile.Tile
+import java.awt.Graphics
+
+class Tree(tx: Float, ty: Float) extends StaticEntity(tx, ty, Tile.tileWidth, Tile.tileHeight*2) {
+  def tick() = {
+    
+  }
+  def render(g: Graphics) = {
+    
+  }
+}
